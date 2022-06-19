3c7f6a4e2aa9d0075388367bf1ea617d56018830
Gabriel Zidane <batmantdk90@gmail.com>
2022-06-18 22:48:40 -0500

    Entonces que quiere el cuate ese? 1

--- js/index.js
+++ js/index.js
@@ -1,61 +1,60 @@
 $('document').ready(function()
 {
-    $('#pagepiling').pagepiling({
-        direction: 'vertical',
-        afterRender: function()
-        {
-            $('.btnArriba, .btnAbajoMovil img').click(function(event)
-            {
-                event.stopPropagation();
-                $.fn.pagepiling.moveTo(1);
-            });
-            $('.btnAbajo, .btnArribaMovil img').click(function(event)
-            {
-                event.stopPropagation();
-                $.fn.pagepiling.moveTo(2);
+    // $('#pagepiling').pagepiling({
+    //     direction: 'vertical',
+    //     afterRender: function()
+    //     {
+    //
+    //     },
+    // });
 
-            });
+    $('.btnArriba, .btnAbajoMovil img').click(function(event)
+    {
+        event.stopPropagation();
+        $.fn.pagepiling.moveTo(1);
+    });
+    $('.btnAbajo, .btnArribaMovil img').click(function(event)
+    {
+        event.stopPropagation();
+        $.fn.pagepiling.moveTo(2);
 
-            $('.btnArribaMovil').click(function()
-            {
-                $.fn.pagepiling.moveTo(1);
-            });
-            $('.btnAbajoMovil').click(function()
-            {
-                $.fn.pagepiling.moveTo(2);
-
-            });
-
-            if($(window).width() >= 575)
-            {
-                $('.botonesCont button').css({ height: $('.botonesCont').width() + 'px' });
-                $('.cabeceraAbajo a img').css({ width: ($(window).width() * 0.023) + 'px', height: ($(window).width() * 0.023) + 'px' });
-                $('.contenidoArriba img').css({ top: ($(window).height() / 2) - ($('.contenidoArriba img').height() / 2) + 'px' });
-                $('.contenidoArriba2 img').css({ top: ($(window).height() / 2) - ($('.contenidoArriba img').height() / 2) + 'px' });
-            }
-
-
-            // setTimeout(function()
-            // {
-            //     $('.seccion1Der .textoInicio').each(function(k)
-            //     {
-            //         let el = $(this);
-            //
-            //         setTimeout(function()
-            //         {
-            //             el.addClass('fadeUp');
-            //         }, 600 * k);
-            //     });
-            // }, 500);
-        },
     });
 
+    $('.btnArribaMovil').click(function()
+    {
+        $.fn.pagepiling.moveTo(1);
+    });
+    $('.btnAbajoMovil').click(function()
+    {
+        $.fn.pagepiling.moveTo(2);
+
+    });
+
+    if($(window).width() >= 575)
+    {
+        $('.botonesCont button').css({ height: $('.botonesCont').width() + 'px' });
+        $('.cabeceraAbajo a img').css({ width: ($(window).width() * 0.023) + 'px', height: ($(window).width() * 0.023) + 'px' });
+        $('.contenidoArriba img').css({ top: ($(window).height() / 2) - ($('.contenidoArriba img').height() / 2) + 'px' });
+        $('.contenidoArriba2 img').css({ top: ($(window).height() / 2) - ($('.contenidoArriba img').height() / 2) + 'px' });
+    }
 
 
-
+    // setTimeout(function()
+    // {
+    //     $('.seccion1Der .textoInicio').each(function(k)
+    //     {
+    //         let el = $(this);
+    //
+    //         setTimeout(function()
+    //         {
+    //             el.addClass('fadeUp');
+    //         }, 600 * k);
+    //     });
+    // }, 500);
 
 
 });
+
 
 $(window).resize(function()
 {
